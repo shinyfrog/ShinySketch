af1eec4ea8785e2593bb29d50161fc5df55d5dfc
matteorattotti <matteo.rattotti@gmail.com>
2017-04-04 15:42:59 +0200

    utility method

--- Shiny Sketch/SFSketch/SFSketchPenTool.m
+++ Shiny Sketch/SFSketch/SFSketchPenTool.m
@@ -41,6 +41,11 @@ CGPoint midPoint(CGPoint p1, CGPoint p2)
     return CGPointMake((p1.x + p2.x) * 0.5, (p1.y + p2.y) * 0.5);
 }
 
+- (CGFloat) distanceBetweenPoint: (CGPoint) p1 secondPoint: (CGPoint) p2
+{
+    return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
 - (CGRect) boundsForLine: (SFSketchLine *) line
 {
     return CGRectInset(line.lineBounds, -self.tipSize, -self.tipSize);
@@ -78,13 +83,13 @@ CGPoint midPoint(CGPoint p1, CGPoint p2)
         CGContextMoveToPoint(context, mid1.x, mid1.y);
         CGContextAddQuadCurveToPoint(context, previousPoint1.location.x, previousPoint1.location.y, mid2.x, mid2.y);
         CGContextStrokePath(context);
-        
+
         previousPoint2 = previousPoint1;
         previousPoint1 = currentPoint;
         
-        //[self drawPoint:mid1 inContext:context color:[UIColor greenColor].CGColor];
-        //[self drawPoint:mid2 inContext:context color:[UIColor blueColor].CGColor];
-        //[self drawPoint:currentPoint.location inContext:context color:[UIColor redColor].CGColor];
+//        [self drawPoint:mid1 inContext:context color:[[UIColor greenColor] colorWithAlphaComponent:0.5].CGColor];
+//        [self drawPoint:mid2 inContext:context color:[[UIColor blueColor] colorWithAlphaComponent:0.5].CGColor];
+//        [self drawPoint:currentPoint.location inContext:context color:[[UIColor redColor] colorWithAlphaComponent:0.5].CGColor];
         
     }];
 }
